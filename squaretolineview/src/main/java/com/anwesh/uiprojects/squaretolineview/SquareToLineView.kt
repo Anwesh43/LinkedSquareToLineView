5db593edbd6443934a44b37c3e1bb6344523c9b0
Anwesh43 <anweshthecool0@gmail.com>
2018-08-09 02:55:37 +0530

    SquareToLineView class

--- squaretolineview/src/main/java/com/anwesh/uiprojects/squaretolineview/SquareToLineView.kt
+++ squaretolineview/src/main/java/com/anwesh/uiprojects/squaretolineview/SquareToLineView.kt
@@ -11,3 +11,23 @@ import android.graphics.Paint
 import android.graphics.Canvas
 
 val nodes : Int = 5
+
+class SquareToLineView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+
+    
+}
