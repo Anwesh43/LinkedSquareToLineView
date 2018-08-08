d5a3cd5a672a088ede9fa081fd78ba1dd0d320ba
Anwesh43 <anweshthecool0@gmail.com>
2018-08-09 03:15:32 +0530

    adding STLNode class

--- squaretolineview/src/main/java/com/anwesh/uiprojects/squaretolineview/SquareToLineView.kt
+++ squaretolineview/src/main/java/com/anwesh/uiprojects/squaretolineview/SquareToLineView.kt
@@ -100,4 +100,50 @@ class SquareToLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class STLNode(var i : Int, val state : State = State()) {
+
+        private var next : STLNode? = null
+
+        private var prev : STLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = STLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSTLNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : STLNode {
+            var curr : STLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+
+    }
 }
